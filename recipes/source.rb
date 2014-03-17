f737e243b97a2b9960515fee89c3820f0cd758e0
Vitaly[AVVS] <v@aminev.me>
2014-03-17 14:40:59 +0400

    correct version

--- recipes/source.rb
+++ recipes/source.rb
@@ -33,15 +33,15 @@ execute 'build-logstash' do
   # This variant is useful for troubleshooting stupid environment problems
   command "make clean && make tarball --debug > /tmp/make.log 2>&1"
   action :run
-  creates "#{node['logstash']['basedir']}/source/build/logstash-#{logstash_version}.tar.gz"
-  not_if "test -f #{node['logstash']['basedir']}/source/build/logstash-#{logstash_version}.tar.gz"
+  creates "#{node['logstash']['basedir']}/source/build/logstash-v#{logstash_version}.tar.gz"
+  not_if "test -f #{node['logstash']['basedir']}/source/build/logstash-v#{logstash_version}.tar.gz"
   notifies :run, 'execute[extract-logstash]', :immediately
 end
 
 execute 'extract-logstash' do
   cwd "#{node['logstash']['basedir']}/source/build"
   user node['logstash']['user']
-  command "rm -rf #{node['logstash']['server']['home']}/* && tar zxvf logstash-#{logstash_version}.tar.gz #{node['logstash']['server']['home']}"
+  command "rm -rf #{node['logstash']['server']['home']}/* && tar zxvf logstash-v#{logstash_version}.tar.gz #{node['logstash']['server']['home']}"
   action :nothing
   notifies :restart, 'service[logstash_server]'
 end
