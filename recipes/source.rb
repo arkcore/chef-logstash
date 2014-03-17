fe173f93cf4a7099d2a0df26d0ce4d26290c3d02
Vitaly[AVVS] <v@aminev.me>
2014-03-17 15:32:25 +0400

    version bananza

--- recipes/source.rb
+++ recipes/source.rb
@@ -7,7 +7,7 @@ include_recipe 'logstash::default'
 
 package 'wget'
 
-logstash_version = node['logstash']['source']['sha'] || "v#{node['logstash']['server']['version']}"
+logstash_version = node['logstash']['source']['sha'] || node['logstash']['server']['version']
 
 directory "#{node['logstash']['basedir']}/source" do
   action :create
@@ -18,7 +18,7 @@ end
 
 git "#{node['logstash']['basedir']}/source" do
   repository node['logstash']['source']['repo']
-  reference logstash_version
+  reference "v#{logstash_version}"
   action :sync
   user node['logstash']['user']
   group node['logstash']['group']
@@ -31,17 +31,17 @@ execute 'build-logstash' do
   )
   user node['logstash']['user']
   # This variant is useful for troubleshooting stupid environment problems
-  command "make clean && make tarball --debug > /tmp/make.log 2>&1"
+  command "make clean && make tarball --debug"
   action :run
-  creates "#{node['logstash']['basedir']}/source/build/logstash-v#{logstash_version}.tar.gz"
-  not_if "test -f #{node['logstash']['basedir']}/source/build/logstash-v#{logstash_version}.tar.gz"
+  creates "#{node['logstash']['basedir']}/source/build/logstash-#{logstash_version}.tar.gz"
+  not_if "test -f #{node['logstash']['basedir']}/source/build/logstash-#{logstash_version}.tar.gz"
   notifies :run, 'execute[extract-logstash]', :immediately
 end
 
 execute 'extract-logstash' do
   cwd "#{node['logstash']['basedir']}/source/build"
   user node['logstash']['user']
-  command "rm -rf #{node['logstash']['server']['home']}/* && tar zxvf logstash-v#{logstash_version}.tar.gz #{node['logstash']['server']['home']}"
+  command "rm -rf #{node['logstash']['server']['home']}/* && tar zxvf logstash-#{logstash_version}.tar.gz #{node['logstash']['server']['home']}"
   action :nothing
   notifies :restart, 'service[logstash_server]'
 end
